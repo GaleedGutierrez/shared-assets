5a9f9ef76fa4c0aaa3df5a7300b4c333f209b842
galeed gutierrez <galeedeveloper@gmail.com>
2025-07-17 18:31:19 -0300

    feat: add project model interfaces for CoursePlatform, ImagesProject, and Project

---
+++ src/models/project.model.ts
@@ -0,0 +1,27 @@
+export interface CoursePlatform {
+	name: string;
+	platform: string;
+	url: string;
+	img: string;
+}
+
+export interface ImagesProject {
+	small: string;
+	medium: string;
+	large: string;
+}
+
+export interface Project {
+	id: number;
+	name: string;
+	technologies: string[];
+	technologiesIcons: string[];
+	repository: string;
+	url: string;
+	images: ImagesProject;
+	date: Date;
+	course: CoursePlatform;
+	description: string;
+	author: string;
+	urlBlog: string;
+}
