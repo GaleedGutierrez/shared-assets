f6140b85e8b0ecb902821e6e41eed30387a9b305
galeed gutierrez <galeedeveloper@gmail.com>
2025-08-06 17:48:53 -0300

    feat: add asset type declarations for module importability

---
+++ src/assets.ts
@@ -0,0 +1,4 @@
+// Asset type declarations for consuming projects
+// Import this module in your project to enable SVG and other asset imports
+
+export { assetModuleDeclarations } from './assets/index.js';
