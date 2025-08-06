d84f7bd244ad35d4f3ea679c45e595e297d26d58
galeed gutierrez <galeedeveloper@gmail.com>
2025-08-06 17:50:26 -0300

    feat: migrate asset type declarations to global.d.ts for improved module support

---
+++ src/global.d.ts
@@ -0,0 +1,51 @@
+// Global asset type declarations
+// This file will be included in the distributed package to provide
+// type support for asset imports in consuming projects
+
+declare module '*.webp' {
+	const value: string;
+
+	export default value;
+}
+
+declare module '*.svg' {
+	const value: string;
+
+	export default value;
+}
+
+declare module '*.png' {
+	const value: string;
+
+	export default value;
+}
+
+declare module '*.jpg' {
+	const value: string;
+
+	export default value;
+}
+
+declare module '*.jpeg' {
+	const value: string;
+
+	export default value;
+}
+
+declare module '*.gif' {
+	const value: string;
+
+	export default value;
+}
+
+declare module '*.avif' {
+	const value: string;
+
+	export default value;
+}
+
+declare module '*.ico' {
+	const value: string;
+
+	export default value;
+}
